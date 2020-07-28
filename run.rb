bf4cdd6b81aa946eddb426e740128533ba4d9cff
BellaEvalle <bellaevalle@yahoo.com>
2020-07-28 11:00:44 -0400

    updated

--- run.rb
+++ run.rb
@@ -62,22 +62,20 @@ zoos = {
 
 # Central Park Zoo has just received 4 wolves. Alter the `zoos` information to reflect this new change:
 # Put this hash: { species: "Wolf", count: 4 } in the array under the `:animals` key in Central Park Zoo
-zoo[:"Central Park Zoo"][:animals] << { species: "Wolf", count: 4 }
-end 
-
+zoos[:"Central Park Zoo"][:animals] << { species: "Wolf", count: 4 }
 
 # The 2 penguins in the Bronx Zoo just had a baby. Alter the `zoos` information to reflect this new change:
 # Increment the number for the `:count` key in the "Penguin" hash in the Bronx Zoo array by 1. 
 # You can assume that the animal at index 0 will always be the "Penguin" hash.
 
-zoos{:Bronx_zoo}[:animals][0]{:count} += 1 
+zoos{:Bronx_zoo}[:animals][0]{:count} << + 1 
 
 
 # Each of the zoos in the city just received 2 pandas. Alter the `zoos` information to reflect this new change:
 # In the array of animals under each Zoo, shovel this hash: { species: "Panda", count: 2 }
 
 zoos[:animals] << { species: "Panda", count: 2 }
-end 
+
 
 
 # Return the number of Tigers at the Bronx Zoo.
@@ -85,19 +83,17 @@ end
 # First find the "Tiger" hash from the array of Animals at the Bronx Zoo and then, access the value under the ":count" key
 
 zoos{:Bronx_Zoo}[:animals][2]{:count}
-end 
+ 
 
 
 # Generalize the process to find the ticket price of a specific zoo.
 # In other words, you're given a `name_of_zoo` variable that is a string.
 # Return the price associated with the `name_of_zoo` variable.
  
-name_of_zoo = "Bronx Zoo"
-name_of_zoo = Bronx_Zoo.find do |zoo_hash|
-    zoo_hash{:price}
-end 
+# name_of_zoo = "Bronx Zoo"
+# name_of_zoo = Bronx_Zoo.find do |zoo_hash|
+#     zoo_hash{:price}
 
-or 
 
 name_of_zoo = zoo.find do |zoo_hash|
     zoo_hash{:price}
@@ -125,6 +121,7 @@ end
 sum = zoo.map do |k, v|
 
 
+
  
 
 # Return an array of all the locations where the zoos are located.
@@ -140,6 +137,9 @@ end
 # The return value should be a hash with two keys: ["Bronx Zoo", "Central Park Zoo"]
 # Consider which higher-level enumerable method(s) you'd use here.
 
+zoos_weekend = zoos.map do |zoo_hash|
+    if zoo_hash{:weekend} == true
+    return zoo_hash
 
 
 
@@ -171,7 +171,3 @@ end
 # Find the zoo with the highest count of any species in it. (Butterflies with the 12 count is the highest)
 # The return value should be an array with two elements: ["Bronx Zoo", { location: "Bronx", ... }]
 # Consider which higher-level enumerable method(s) you'd use here.
-
-
-
-
